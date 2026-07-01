802363ea66b25ee99781d6769ec791a041a4c56a
nataly obregon <natalyvalentina2008@gmail.com>
2026-07-01 14:04:10 -0400

    Guía 3: Rutas funcionando para Login, Home, Dashboards

--- sportclub/src/routes/AppRoutes.jsx
+++ sportclub/src/routes/AppRoutes.jsx
@@ -2,6 +2,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import Home from "../pages/Home";
 import Login from "../pages/Login";
+import Unauthorized from "../pages/Unauthorized";
+
+import UserDashboard from "../pages/user/UserDashboard";
+import CoachDashboard from "../pages/coach/CoachDashboard";
+import AdminDashboard from "../pages/admin/AdminDashboard";
+
+import ProtectedRoute from "./ProtectedRoute";
+import RoleRoute from "./RoleRoute";
+import UserLayout from "../layouts/UserLayout";
 
 function AppRoutes() {
   return (
@@ -9,10 +18,22 @@ function AppRoutes() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
-        
+        <Route path="/unauthorized" element={<Unauthorized />} />
+
+        <Route path="/user" element={<RoleRoute allowedRoles={["user"]}><UserLayout /> </RoleRoute>}>
+            <Route path="dashboard" element={<UserDashboard />} />
+        </Route>
+
+        <Route path="/coach" element={<RoleRoute allowedRoles={["coach"]}><UserLayout /> </RoleRoute>}>
+            <Route path="dashboard" element={<CoachDashboard />} />
+        </Route>
+
+        <Route path="/admin" element={<RoleRoute allowedRoles={["admin"]}><UserLayout /> </RoleRoute>}>
+            <Route path="dashboard" element={<AdminDashboard />} />
+        </Route>
       </Routes>
     </BrowserRouter>
-  );
+  )
 }
 
-export default AppRoutes;
+export default AppRoutes
